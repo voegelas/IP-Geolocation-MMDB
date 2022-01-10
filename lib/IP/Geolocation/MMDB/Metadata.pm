9591e3c76a86870eb999371d6491333ce039ad29
Andreas Vögele <andreas@andreasvoegele.com>
2022-01-10 15:12:42 +0100

    Bump version

--- lib/IP/Geolocation/MMDB/Metadata.pm
+++ lib/IP/Geolocation/MMDB/Metadata.pm
@@ -6,7 +6,7 @@ use 5.016;
 use warnings;
 use utf8;
 
-our $VERSION = 0.007;
+our $VERSION = 0.008;
 
 sub new {
   my ($class, %attrs) = @_;
@@ -81,7 +81,7 @@ IP::Geolocation::MMDB::Metadata - Metadata from a MaxMind DB file
 
 =head1 VERSION
 
-version 0.007
+version 0.008
 
 =head1 SYNOPSIS
 
